3b0f62f6966d97dbc6138ec0427b6f592f826d83
jordi <jordi.fernandez@ikasle.egibide.org>
2024-05-10 13:33:33 +0200

    ergergg

--- Programacion/CESJava/src/controlador/baseDeDatos/TablaJornadas.java
+++ Programacion/CESJava/src/controlador/baseDeDatos/TablaJornadas.java
@@ -3,6 +3,7 @@ package controlador.baseDeDatos;
 import modelo.Competiciones;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class TablaJornadas {
     private Connection con;
@@ -13,6 +14,24 @@ public class TablaJornadas {
 
     public void ultimaJornada(Competiciones com) {
         int numCompe = com.getCodCompe();
-        String plantilla = "select max()"
+        String plantilla = "SELECT e.cod_jornada, e.hora, e.fecha, e.resultado, \n" +
+                "    e.cod_equipo_local, e.cod_equipo_visitante \n" +
+                "FROM \n" +
+                "    competiciones c \n" +
+                "    JOIN jornadas j ON j.cod_compe = c.cod_compe \n" +
+                "    JOIN enfrentamientos e ON e.cod_jornada = j.cod_jornadas \n" +
+                "WHERE \n" +
+                "    j.num_jornada = (\n" +
+                "        SELECT \n" +
+                "            MAX(j.num_jornada) \n" +
+                "        FROM \n" +
+                "            jornadas j \n" +
+                "            JOIN competiciones c ON j.cod_compe = c.cod_compe \n" +
+                "        WHERE \n" +
+                "            c.nombre = ? \n" +
+                "    )\n" +
+                "    AND c.nombre = ? ;" +
+                "";
+        PreparedStatement pre = con.preparedStatement(plantilla);
     }
 }
