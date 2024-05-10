df0006ced8607a493b02b151275b9004112c80f8
jordi <jordi.fernandez@ikasle.egibide.org>
2024-05-10 11:29:12 +0200

    etc

--- Programacion/CESJava/src/controlador/baseDeDatos/ControladorBaseDeDatos.java
+++ Programacion/CESJava/src/controlador/baseDeDatos/ControladorBaseDeDatos.java
@@ -29,47 +29,30 @@ public class ControladorBaseDeDatos {
             Class.forName("oracle.jdbc.OracleDriver");
             con = DriverManager.getConnection(url, user, passwd);
             System.out.println("Conexión exitosa a la base de datos");
+
         } catch (SQLException e) {
             System.out.println("Error al conectar a la base de datos: " + e.getMessage());
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
 
-        /* ----------------- Conexion con la BD Clase PC ----------------- */
-
-//        String url = "jdbc:oracle:thin:@172.20.225.114:1521:orcl";
-//        String user = "eqdaw04";
-//        String passwd = "eqdaw04";
-//
-//        try {
-//            Class.forName("oracle.jdbc.OracleDriver");
-//            con = DriverManager.getConnection(url, user, passwd);
-//            System.out.println("Conexión exitosa a la base de datos");
-//        } catch (SQLException e) {
-//            System.out.println("Error al conectar a la base de datos: " + e.getMessage());
-//        } catch (ClassNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
-
 
         /* ----------------- Conexion con la BD Local Casa (Test)  ----------------- */
 
+            /*String url = "jdbc:oracle:thin:@localhost:1521:XE";
+            String user = "userproyecto";
+            String passwd = "userproyecto";
 
-//            String url = "jdbc:oracle:thin:@localhost:1521:XE";
-//            String user = "userproyecto";
-//            String passwd = "userproyecto";
-//
-//            try {
-//                Class.forName("oracle.jdbc.OracleDriver");
-//                con = DriverManager.getConnection(url, user, passwd);
-//                System.out.println("Conexión exitosa a la base de datos");
-//
-//            } catch (SQLException e) {
-//                System.out.println("Error al conectar a la base de datos: " + e.getMessage());
-//            } catch (ClassNotFoundException e) {
-//                throw new RuntimeException(e);
-//            }
-
+            try {
+                Class.forName("oracle.jdbc.OracleDriver");
+                con = DriverManager.getConnection(url, user, passwd);
+                System.out.println("Conexión exitosa a la base de datos");
+                
+            } catch (SQLException e) {
+                System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }*/
     }
 
     /* ----------------- Metodo Inicializacion Tablas  ----------------- */
