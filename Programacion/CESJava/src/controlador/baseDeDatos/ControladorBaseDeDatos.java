5f2c23856f1b6b2edcfa7cd3bf2616788a76b05f
Jon <jon.garay@ikasle.egibide.org>
2024-05-14 09:52:02 +0200

    A

--- Programacion/CESJava/src/controlador/baseDeDatos/ControladorBaseDeDatos.java
+++ Programacion/CESJava/src/controlador/baseDeDatos/ControladorBaseDeDatos.java
@@ -102,8 +102,6 @@ public class ControladorBaseDeDatos {
         return te.consultaEquipo(nombreEq).toString();
     }
 
-
-
     public void altaStaff(Staff staff) {
         ts.altaStaff(staff);
     }
