dfbc26fd07577d0a042d941517c11d346c9488c0
Adrian Lopez <adrian.lopez@ikasle.egibide.org>
2024-05-14 21:05:29 +0200

    Procedimiento, DTD, XSD y XML generado por procedimiento de Todas Jornad
    Procedimiento SQL, XML Generado para DTD, XML Generado para XSD, DTD, XSD

--- Lenguaje de Marcas/Fase 4/prcedimiento_resultado_todas_jornadas.sql
+++ Lenguaje de Marcas/Fase 4/prcedimiento_resultado_todas_jornadas.sql
@@ -1,3 +1,5 @@
+SET SERVEROUTPUT ON;
+
 DROP TABLE temp_jornadas_tab;
 
 CREATE TABLE temp_jornadas_tab (
@@ -6,12 +8,13 @@ CREATE TABLE temp_jornadas_tab (
 
 DECLARE
     v_xml_data CLOB;
+    resultado CLOB; -- Declare resultado variable
 BEGIN
     -- Generar XML con los datos de todas las jornadas
     SELECT XMLElement("Jornadas",
                XMLAgg(
                    XMLElement("Jornada",
-                       XMLAttributes(j.cod_jornadas AS "Codigo_Jornada", j.num_jornada AS "Numero_Jornada"),
+                       XMLAttributes(j.num_jornada AS "Numero_Jornada", j.cod_compe AS "Codigo_Competencion", j.cod_jornadas AS "Codigo_Jornada"),
                        XMLAgg(
                            XMLElement("Enfrentamiento",
                                XMLForest(
@@ -21,7 +24,7 @@ BEGIN
                                    e.resultado AS "Ganador"
                                )
                            )
-                       ) AS "Enfrentamientos"
+                       )
                    )
                )
            ).getClobVal()
@@ -30,12 +33,15 @@ BEGIN
     LEFT JOIN enfrentamientos e ON j.cod_jornadas = e.cod_jornada
     LEFT JOIN equipos el ON e.cod_equipo_local = el.cod_equipo
     LEFT JOIN equipos ev ON e.cod_equipo_visitante = ev.cod_equipo
-    GROUP BY j.cod_jornadas, j.num_jornada
-    ORDER BY j.cod_jornadas;
+    GROUP BY j.cod_jornadas, j.num_jornada, j.cod_compe;
 
-    -- Insertar el resultado en la tabla temporal
-    INSERT INTO temp_jornadas_tab (xml_data) VALUES (v_xml_data);
-
-    -- Confirmar la transacción
-    COMMIT;
+    -- Concatenar el encabezado XML y el DTD al resultado
+    resultado := '<?xml version=''1.0'' encoding=''UTF-8'' ?>' || '<!DOCTYPE Jornadas SYSTEM "resultados_todas_jornadas.dtd">' || v_xml_data;
+    DBMS_OUTPUT.PUT_LINE(resultado);
+    
+    -- Insertar el resultado en la tabla
+    INSERT INTO temp_jornadas_tab (xml_data) VALUES (resultado);
+    
+    COMMIT; -- Realiza la inserción de manera permanente
 END;
+/
