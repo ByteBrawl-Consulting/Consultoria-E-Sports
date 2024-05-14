bc15b2f7b4cf03c6fd6146fd3faf7c5c104fcce7
Jon <jon.garay@ikasle.egibide.org>
2024-05-14 14:09:15 +0200

    Staff

--- Programacion/CESJava/src/controlador/juegos/ControladorJuego.java
+++ Programacion/CESJava/src/controlador/juegos/ControladorJuego.java
@@ -1,4 +1,55 @@
 package controlador.juegos;
 
+import controlador.ControladorVista;
+import controlador.equipos.ControladorEquipo;
+import view.VentanaJuegos;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class ControladorJuego {
+    private VentanaJuegos vj;
+    private ControladorVista cv;
+
+    public ControladorJuego(ControladorVista cv) {
+        vj = new VentanaJuegos();
+        mostar();
+        vj.bAceptarAl(new bAceptar());
+        vj.bSalirAL(new bSalir());
+        vj.bRbAltaAL(new bAlta());
+        vj.bRbBajaAL(new bBaja());
+        vj.bRbModiAl(new bModi());
+        vj.bRbConsultaAL(new bConsulta());
+        this.cv = cv;
+    }
+    private void mostar() {
+        vj.setVisible(true);
+    }
+    private class bAlta implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            vj.eleccionAlta();
+        }
+    }
+
+    private class bBaja implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            vj.eleccionBaja();
+        }
+    }
+
+    private class bModi implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            vj.eleccionModi();
+        }
+    }
+
+    private class bConsulta implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            vj.eleccionConsulta();
+        }
+    }
 }
