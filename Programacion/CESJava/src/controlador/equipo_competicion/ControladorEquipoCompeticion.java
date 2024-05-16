17d69dc5817f69dc961bf9084dadab787dfac194
Adrian Lopez <adrian.lopez@ikasle.egibide.org>
2024-05-16 10:58:59 +0200

    imports

--- Programacion/CESJava/src/controlador/equipo_competicion/ControladorEquipoCompeticion.java
+++ Programacion/CESJava/src/controlador/equipo_competicion/ControladorEquipoCompeticion.java
@@ -2,6 +2,7 @@ package controlador.equipo_competicion;
 
 import controlador.ControladorVista;
 import controlador.login.ControladorLogin;
+import modelo.Equipo;
 import view.VentanaAsociacionEquipoCompe;
 import view.VentanaLogin;
 
@@ -10,6 +11,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ControladorEquipoCompeticion {
     private VentanaAsociacionEquipoCompe vaec;
