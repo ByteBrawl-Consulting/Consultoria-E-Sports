a94ae3888fa7d52aee3486381ae810a102071815
jordi <jordi.fernandez@ikasle.egibide.org>
2024-05-17 12:22:11 +0200

    copia seguridad

--- Programacion/CESJava/src/view/VentanaClasificacionAdmin.java
+++ Programacion/CESJava/src/view/VentanaClasificacionAdmin.java
@@ -1,6 +1,7 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class VentanaClasificacionAdmin extends JFrame {
 
@@ -8,4 +9,10 @@ public class VentanaClasificacionAdmin extends JFrame {
     private JPanel pPirncipal;
     private JComboBox cbClasi;
     private JTextArea taClasi;
+
+    public VentanaClasificacionAdmin() throws HeadlessException {
+        super("Clasificacion");
+        setSize(1920,1080);
+
+    }
 }
