2bf329aa7dee798ebf678922ea57b459497f8b48
jordi <jordi.fernandez@ikasle.egibide.org>
2024-05-17 11:55:16 +0200

    seguridad

---
+++ Programacion/CESJava/src/view/VentanaClasificacionAdmin.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class VentanaClasificacionAdmin extends JFrame {
+
+
+    private JPanel pPirncipal;
+    private JComboBox cbClasi;
+    private JTextArea taClasi;
+}
