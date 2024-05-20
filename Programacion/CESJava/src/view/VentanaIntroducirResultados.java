9788483221f602cf36f027ae2e0d95de11bb95eb
Adrian Lopez <adrian.lopez@ikasle.egibide.org>
2024-05-20 18:53:54 +0200

    ventana introducir resultados codificada

--- Programacion/CESJava/src/view/VentanaIntroducirResultados.java
+++ Programacion/CESJava/src/view/VentanaIntroducirResultados.java
@@ -1,6 +1,7 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class VentanaIntroducirResultados extends JFrame{
     private JPanel pPrincipal;
@@ -11,17 +12,33 @@ public class VentanaIntroducirResultados extends JFrame{
     private JRadioButton modificaciónRadioButton;
     private JComboBox cbCompetición;
     private JTextField tfJornada;
-    private JTextArea taConsulta;
+    private JTextArea taConsultaAlta;
     private JTextField tfCodEnfrentamiento;
     private JTextField tfEquipoGanador;
     private JButton aceptarButton;
     private JButton salirButton;
+    private JTextArea taConsultaModi;
+    private JPanel pModi;
+    private JTextField tfJornadaModi;
+    private JTextField tfCodEnfreModi;
+    private JTextField tfEquiGanadorModi;
+    private JComboBox cbCompeticionModi;
 
     public VentanaIntroducirResultados() {
         super("Ventana Introducir Resultados");
         setContentPane(pPrincipal);
         setSize(500, 600);
         setLocationRelativeTo(null);
+    }
+
+    public void eleccionAlta() {
+        pAlta.setVisible(true);
+        pModi.setVisible(false);
+    }
+
+    public void eleccionModi() {
+        pAlta.setVisible(false);
+        pModi.setVisible(true);
     }
 
     public JRadioButton getAltaRadioButton() {
@@ -40,7 +57,7 @@ public class VentanaIntroducirResultados extends JFrame{
         this.modificaciónRadioButton = modificaciónRadioButton;
     }
 
-    public JComboBox getCbCompetición() {
+    public JComboBox getCbCompeticion() {
         return cbCompetición;
     }
 
@@ -56,12 +73,12 @@ public class VentanaIntroducirResultados extends JFrame{
         this.tfJornada = tfJornada;
     }
 
-    public JTextArea getTaConsulta() {
-        return taConsulta;
+    public JTextArea getTaConsultaAlta() {
+        return taConsultaAlta;
     }
 
-    public void setTaConsulta(JTextArea taConsulta) {
-        this.taConsulta = taConsulta;
+    public void setTaConsultaAlta(JTextArea taConsultaAlta) {
+        this.taConsultaAlta = taConsultaAlta;
     }
 
     public JTextField getTfCodEnfrentamiento() {
@@ -78,5 +95,77 @@ public class VentanaIntroducirResultados extends JFrame{
 
     public void setTfEquipoGanador(JTextField tfEquipoGanador) {
         this.tfEquipoGanador = tfEquipoGanador;
+    }
+
+    public JTextArea getTaConsultaModi() {
+        return taConsultaModi;
+    }
+
+    public void setTaConsultaModi(JTextArea taConsultaModi) {
+        this.taConsultaModi = taConsultaModi;
+    }
+
+    public JTextField getTfJornadaModi() {
+        return tfJornadaModi;
+    }
+
+    public void setTfJornadaModi(JTextField tfJornadaModi) {
+        this.tfJornadaModi = tfJornadaModi;
+    }
+
+    public JTextField getTfCodEnfreModi() {
+        return tfCodEnfreModi;
+    }
+
+    public void setTfCodEnfreModi(JTextField tfCodEnfreModi) {
+        this.tfCodEnfreModi = tfCodEnfreModi;
+    }
+
+    public JTextField getTfEquiGanadorModi() {
+        return tfEquiGanadorModi;
+    }
+
+    public void setTfEquiGanadorModi(JTextField tfEquiGanadorModi) {
+        this.tfEquiGanadorModi = tfEquiGanadorModi;
+    }
+
+    public JComboBox getCbCompeticionModi() {
+        return cbCompeticionModi;
+    }
+
+    public void setCbCompeticionModi(JComboBox cbCompeticionModi) {
+        this.cbCompeticionModi = cbCompeticionModi;
+    }
+
+    public void bRbAltaAL(ActionListener listener) {
+        altaRadioButton.addActionListener(listener);
+    }
+
+    public void bRbModiAl(ActionListener listener) {
+        modificaciónRadioButton.addActionListener(listener);
+    }
+
+    public void bAceptarAL(ActionListener listener) {
+        aceptarButton.addActionListener(listener);
+    }
+
+    public void bSalirAL(ActionListener listener) {
+        salirButton.addActionListener(listener);
+    }
+
+    public JPanel getpAlta() {
+        return pAlta;
+    }
+
+    public void setpAlta(JPanel pAlta) {
+        this.pAlta = pAlta;
+    }
+
+    public JPanel getpModi() {
+        return pModi;
+    }
+
+    public void setpModi(JPanel pModi) {
+        this.pModi = pModi;
     }
 }
