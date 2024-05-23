75a1549e50d35a1d9140f8eddd17edce07d0f703
jordi <jordi.fernandez@ikasle.egibide.org>
2024-05-23 12:17:57 +0200

    efeff

--- Programacion/CESJava/test/controlador/equipos/ControladorEquipoTest.java
+++ Programacion/CESJava/test/controlador/equipos/ControladorEquipoTest.java
@@ -1,15 +1,5 @@
 package controlador.equipos;
 
-import controlador.ControladorPrincipal;
-import controlador.ControladorVista;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-import java.awt.Color;
-import javax.swing.JTextField;
-import java.awt.event.ActionEvent;
-
 class ControladorEquipoTest {
 
 }
