b6e4975c98141226cace5b9b53d6d8d090fd1efc
Adrian Lopez <adrian.lopez@ikasle.egibide.org>
2024-05-24 09:08:04 +0200

    JavaDoc Generado

--- Programacion/CESJava/src/Main.java
+++ Programacion/CESJava/src/Main.java
@@ -1,5 +1,16 @@
 import controlador.ControladorPrincipal;
 
+/**
+ * Clase principal para iniciar la aplicación de gestión de competiciones deportivas.
+ * Esta clase crea una instancia de ControladorPrincipal y lanza la aplicación.
+ *
+ * @version Final
+ *
+ * @author Adrián  López
+ * @author Jon Garay
+ * @author Jordi Fernandez
+ */
+
 public class Main {
     public static void main(String[] args) {
         ControladorPrincipal cp = new ControladorPrincipal();
